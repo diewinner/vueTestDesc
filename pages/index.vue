8dfd9f5d7b09844535e071e9ab3fd4faa154cea5
diewinner <skipper.ilnur1912@gmail.com>
2023-12-11 12:00:03 +0300

    second commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main_container">
+    <welcome/>
+    <about/>
+    <favorites/>
+    <coffee-shop/>
+    <contacts/>
+    <library-cards/>
+    <v-footer/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+
+}
+</script>
+<style>
+
+
+</style>
